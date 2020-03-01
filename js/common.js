f380a6edf646ab34300045cb2e4800b018cf27ad
zhoufight <374147621@qq.com>
2020-03-01 20:00:54 +0800

    完成首页

--- js/common.js
+++ js/common.js
@@ -13,10 +13,10 @@ function adaptive() {
   }
   $(function () { 
     var scroTop = $(window).scrollTop();
+    console.log('scroltiop:',scroTop)
     //为当前窗口添加滚动条滚动事件（适用于所有可滚动的元素和 window 对象（浏览器窗口））
     $(window).scroll(function(){
       //创建一个变量存储当前窗口下移的高度
-    var scroTop = $(window).scrollTop();
     //判断当前窗口滚动高度
     //如果大于100，则显示顶部元素，否则隐藏顶部元素
     if(scroTop>100){
