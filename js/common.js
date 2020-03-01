29807a316991e79f00b0a1347d14396d15b93f85
zhoufight <374147621@qq.com>
2020-03-01 17:39:20 +0800

    完成footer

--- js/common.js
+++ js/common.js
@@ -11,3 +11,25 @@ function adaptive() {
     e(), window.addEventListener("resize", e);
     }();
   }
+  $(function () { 
+    var scroTop = $(window).scrollTop();
+    //为当前窗口添加滚动条滚动事件（适用于所有可滚动的元素和 window 对象（浏览器窗口））
+    $(window).scroll(function(){
+      //创建一个变量存储当前窗口下移的高度
+    var scroTop = $(window).scrollTop();
+    //判断当前窗口滚动高度
+    //如果大于100，则显示顶部元素，否则隐藏顶部元素
+    if(scroTop>100){
+      $('.backtop').fadeIn(500);
+    }else{
+      $('.backtop').fadeOut(500);
+    }
+    );
+          
+    //为返回顶部元素添加点击事件
+    $('.backtop').click(function(){
+    //将当前窗口的内容区滚动高度改为0，即顶部
+    $("html,body").animate({scrollTop:0},"fast");
+    });
+
+   })
